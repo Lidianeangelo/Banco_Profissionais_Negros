85651be14f44618806dedee53f6e10900cd7d818
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-12-07 22:59:50 -0300

    ajustando a modelagem do bd

--- src/model/perfilSchema.js
+++ src/model/perfilSchema.js
@@ -1,86 +1,94 @@
-const mongoose=require("mongoose")
-const Schema=mongoose.Schema
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const perfilSchema=new Schema({
-    _id:{
-        type:mongoose.Types.ObjectId,
-        auto:true,
-        required:true
+const perfilSchema = new Schema({
+    _id: {
+        type: mongoose.Types.ObjectId,
+        auto: true,
+        required: true
     },
-    Nome:{
-        type:String,
-        required:true
+    Nome: {
+        type: String,
+        required: true
     },
-    Gênero:{
-        type:String,
-        required:true
+    Gênero: {
+        type: String,
+        required: true
     },
-    Autodeclaração:{
-        type:String,
-        required:true
+    Autodeclaração: {
+        type: String,
+        required: true
     },
-    Nível:{
-        type:String,
-        required:true
+    Nível: {
+        type: String,
+        required: true
     },
-    Cargo:{
-        type:String,
-        required:true
+    Cargo: {
+        type: String,
+        required: true
     },
-    Área:{
-        type:String,
-        required:true
+    Área: {
+        type: String,
+        required: true
     },
-    Empresa:{
-        type:String,
-        required:true
+    Empresa: {
+        type: String,
+        required: true
     },
-    E_mail:{
-        type:String,
-        required:true
+    E_mail: {
+        type: String,
+        required: true
     },
-    Telefone:{
-        type:String,
-        required:true
+    Telefone: {
+        type: String,
+        required: true
     },
-    Cidade:{
-        type:String,
-        required:true
+    Cidade: {
+        type: String,
+        required: true
     },
-    Estado:{
-        type:String,
-        required:true
+    Estado: {
+        type: String,
+        required: true
     },
-    Bio:{
-        type:String,
-        required:true
+    Bio: {
+        type: String,
+        required: true
     },
-    Portfólio:{
-        type:String,
-        required:true
+    Portfólio: {
+        type: String,
+        required: false
     },
-    Linkedin:{
-        type:String,
-        required:true
+    Linkedin: {
+        type: String,
+        required: false
     },
-    Competencias:{
-        type:String,
-        required:true
+    Competencias: {
+        type: String,
+        required: true
     },
-    Trabalhos:{
-        type:String,
-        required:true
+    Trabalhos: {
+        type: String,
+        required: false
     },
-    FotoPerfil:{
-        type:String,
-        required:true
+    FotoPerfil: {
+        type: String,
+        required: false
     },
-    tag:{
-        type:String,
-        required:true
-    }
-})
+    tag: {
+        type: String,
+        required: true
+    },
+    
 
-const perfilCollection=mongoose.model("perfil",perfilSchema)
+},
+{
+    collection: "perfil",
+    versionKey: false
 
-module.exports=perfilCollection
+}
+)
+
+const perfilCollection = mongoose.model("perfil", perfilSchema)
+
+module.exports = { perfilCollection }
