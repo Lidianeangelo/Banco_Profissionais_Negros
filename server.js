495f67a6991ead8745ac87b211739525bddc5808
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-12-10 22:40:22 -0300

    Alterando port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 dotenv.config()
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 8081
 
 app.listen(PORT, function(){
     console.log(`Servidor rodando na porta ${PORT}`)
