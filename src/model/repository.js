f0ad36661fb9c9eac094d9b400f973f4c5fff886
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-12-11 12:50:09 -0300

    configurando heroku teste4

--- src/model/repository.js
+++ src/model/repository.js
@@ -8,13 +8,23 @@ const DB_URI = process.env.DB_URI || "mongodb+srv://admin_banco:camila02042009@c
 
 const JWT = process.env.JWT
 
-const connect = () => {
+/*const connect = () => {
     mongoose.connect(DB_URI,
         { useNewUrlParser: true })
     const connection = mongoose.connection
 
     connection.on("error", () => console.error("Erro ao se conectar com o servidor"))
     connection.once("open", () => console.log("Servidor conectado, linda!"))
+}*/
+const connect = () => {
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:camila02042009@cluster0.jmolg.mongodb.net/banco_profissionaisnegros?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+})
 }
 
 module.exports = { connect }
