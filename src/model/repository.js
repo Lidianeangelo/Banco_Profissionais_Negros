7dbbec28dfe688b9ac8203a454999af5183052d3
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-12-11 17:51:33 -0300

    subindo projeto heroku

--- src/model/repository.js
+++ src/model/repository.js
@@ -4,11 +4,14 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 //const DB_URL="mongodb://localhost:27017/banco_profissionais"
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://deploy:camila02042009@cluster0.jmolg.mongodb.net/banco_profissionaisnegros?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URI //|| "mongodb+srv://deploy:camila02042009@cluster0.jmolg.mongodb.net/banco_profissionaisnegros?retryWrites=true&w=majority"
 
 const connect = () => {
     mongoose.connect(MONGODB_URI,
-        { useNewUrlParser: true })
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
     const connection = mongoose.connection
 
     connection.on("error", () => console.error("Erro ao se conectar com o servidor"))
