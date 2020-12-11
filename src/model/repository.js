61846c90c92850e55d49764864cc7eaf92ef8144
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-12-10 22:39:08 -0300

    configurando heroku teste]

--- src/model/repository.js
+++ src/model/repository.js
@@ -1,13 +1,20 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const DB_URL="mongodb://localhost:27017/banco_profissionais"
+const dotenv = require("dotenv")
+dotenv.config()
 
-const connect=()=>{
-    mongoose.connect(DB_URL,{useNewUrlParser: true})
-    const connection=mongoose.connection
+//const DB_URL="mongodb://localhost:27017/banco_profissionais"
+const DB_URI = process.env.DB_URI || "mongodb+srv://admin_banco:camila02042009@cluster0.jmolg.mongodb.net/<banco_profissionaisnegros>?retryWrites=true&w=majority"
 
-    connection.on("error", ()=> console.error("Erro ao se conectar com o servidor"))
-    connection.once("open",()=> console.log("Servidor conectado, linda!"))
+const JWT = process.env.JWT
+
+const connect = () => {
+    mongoose.connect(DB_URI,
+        { useNewUrlParser: true })
+    const connection = mongoose.connection
+
+    connection.on("error", () => console.error("Erro ao se conectar com o servidor"))
+    connection.once("open", () => console.log("Servidor conectado, linda!"))
 }
 
-module.exports={connect}
+module.exports = { connect }
